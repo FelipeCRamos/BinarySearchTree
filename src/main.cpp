53f0205dd21bb36fb3c1928f64655863714bbdd7
Felipe Ramos <felipecramos00@gmail.com>
2018-10-27 15:58:35 -0300

    Removed unnecessary print's

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "bst.h"
 #include "parser.h"
 
+#define debug false
+
 enum func_code{
     nth,
     pos,
@@ -14,24 +16,26 @@ enum func_code{
     none
 };
 
+//!< Convert the string function to a func_code
 func_code hashit( std::string const &str_c ){
-    if( str_c == "ENESIMO" ) return nth;
-    if( str_c == "POSICAO" ) return pos;
-    if( str_c == "MEDIANA" ) return median;
-    if( str_c == "CHEIA" ) return isfull;
-    if( str_c == "COMPLETA" ) return iscomp;
-    if( str_c == "IMPRIMA" ) return print;
-    if( str_c == "REMOVA" ) return remov;
-    if( str_c == "INSIRA" ) return insert;
+    if( str_c == "ENESIMO" )    return nth;
+    if( str_c == "POSICAO" )    return pos;
+    if( str_c == "MEDIANA" )    return median;
+    if( str_c == "CHEIA" )      return isfull;
+    if( str_c == "COMPLETA" )   return iscomp;
+    if( str_c == "IMPRIMA" )    return print;
+    if( str_c == "REMOVA" )     return remov;
+    if( str_c == "INSIRA" )     return insert;
 
-    return none;    // none matched
+    return none;    // none matched, probably a invalid command
 }
 
 int main( int argc, char **argv )
 {
-    std::string tree_filename;
-    std::string comm_filename;
-    // Args treating
+    std::string tree_filename;  //!< Path to the tree file
+    std::string comm_filename;  //!< Path to the commands file
+    
+    // Check if the arg's are O.K.!
     if( argc > 2 ){
         tree_filename = argv[1];
         comm_filename = argv[2];
@@ -45,56 +49,60 @@ int main( int argc, char **argv )
     std::ifstream tree_file( tree_filename );
     std::ifstream comm_file( comm_filename );
 
+    // Parse tree file and store on std::vector<int> values
     std::vector< int > values = parse_from( tree_file );
-    std::cout << "Values from " << tree_filename << " parsed.\n";
-    std::vector< std::pair< std::string, int > > commands = parse_commands( comm_file );
-    std::cout << "Commands from " << comm_filename << " parsed.\n";
+    if(debug) std::cout << ">>> Values from " << tree_filename << " parsed.\n";
 
+    // Parse commands file and store on a vector of pairs <std::string, int>
+    std::vector< std::pair< std::string, int > > commands = parse_commands( comm_file );
+    if(debug) std::cout << ">>> Commands from " << comm_filename << " parsed.\n";
+
+    // Close file streams
     tree_file.close();
     comm_file.close();
 
 	bst<int> tree;
-    std::cout << "-> Tree declared successfully\n";
+    if(debug) std::cout << ">>> Tree declared successfully!\n";
 
-    std::cout << "Add: ";
+    // populating values to tree
     for( auto &i : values ){
         tree.add(i);
-        std::cout << i << ", ";
     }
-    std::cout << std::endl;
 
-    std::cout << "Tree by level: ";
+    std::cout << "Tree populated! print by level: ";
     std::cout << "[ " << tree.toString() << "]" << std::endl;
 
+	std::cout << "\n\033[1;32mStarting operations!\033[0m\n";
+    std::cout << "----------------------------------------------\n";
+
     for( auto &i : commands ){
-        std::cout << "command: [" << i.first << "], arg: [" << i.second << "]" << std::endl;
+        if(debug) std::cout << "command: [" << i.first 
+            << "], arg: [" << i.second << "]" << std::endl;
+
         switch(hashit(i.first)){
             case nth:
-				std::cout << "nthElement not implemented\n";
-//				std::cout << "NthElement: " << tree.nthElement( i.second );
+                std::cout << "NthElement: " << tree.nthElement( i.second ) << "\n";
                 break;
             case pos:
-				std::cout << "Position: Not implemented\n";
-//				std::cout << "Position: " << tree.position( i.second ) << "\n";
+                std::cout << "Position: " << tree.position( i.second ) << "\n";
                 break;
             case median:
-				std::cout << "Median: Not implemented\n";
-//                std::cout << "Median element: " << tree.median() << std::endl;
+                std::cout << "Median element: " << tree.median() << std::endl;
                 break;
             case isfull:
                 if( tree.isFull() )
 				{
-					std::cout << "Tree is Full?: YES\n"; 
+					std::cout << "Tree is Full?: True\n"; 
 				} else {
-					std::cout << "Tree is Full?: NO\n";
+					std::cout << "Tree is Full?: False\n";
 				}
                 break;
             case iscomp:
 				if( tree.isComplete() )
 				{
-					std::cout << "Tree is Complete?: YES\n";
+					std::cout << "Tree is Complete?: True\n";
 				} else {
-					std::cout << "Tree is Complete?: NO\n";
+					std::cout << "Tree is Complete?: False\n";
 				}
                 break;
             case print:
@@ -112,41 +120,12 @@ int main( int argc, char **argv )
             case none:
                 std::cout << "Sorry, command " << i.first
 						  << " isn't known!\n Try a valid command.\n";
+                return 1;
         }
     }
 
-	std::cout << "\n\033[1;32mOperations finished successfully!\033[0m\n";
+    std::cout << "----------------------------------------------\n";
+	std::cout << "\033[1;32mOperations finished successfully!\033[0m\n\n";
 
-    std::cout << tree.toString() << std::endl;
-/*
-    for( unsigned int i = 1; i <= tree.size(); i++ ){
-        std::cout << "Nth(" << i << "): " << tree.nthElement(i) << std::endl;
-    }
-
-    // temporary tests
-    int vec[] = { 10, 5, 12, 3, 9, 11, 27, 1, -44};
-    for( auto &i : vec ){
-        std::cout << "\n~ Removing " << i << std::endl;
-        std::cout << "Posicao de " << i << " = " << tree.position(i) << std::endl;
-        tree.remove(i);
-        std::cout << "Result tree: " << tree.toString() << std::endl;
-
-		bst<int> avure;
-		avure.add(10);
-		avure.add(5);
-		avure.add(15);
-		avure.add(12);
-		avure.add(17);
-		avure.add(2);
-		avure.add(8);
-
-		assert( 1 == avure.isFull() );
-		avure.remove(12);
-
-		assert( 0 == avure.isFull() );
-		avure.add(14);
-		assert( 1 == avure.isFull() );
-    }
-*/
 	return 0;
 }
